697ed0b802ebb8c7f9f57cfdf917f9a19d2d9f9f
vppise <vikas@pioniumtech.com>
2020-07-07 13:52:48 +0530

    image added to navbar

--- client/src/component/layout/Navbar.js
+++ client/src/component/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import student from '../../asset/images/student.png'
 // import StudentData from './StudentData';
 
 
@@ -9,7 +10,9 @@ import { Link } from 'react-router-dom';
         <nav>
     <div className="nav-wrapper">
       {/* <a href="/" className="center brand-logo">Student KYC</a> */}
-      <Link to='/' className="center brand-logo">Student KYC</Link>
+      <img src={student} style={{width:50, marginTop: 5, marginLeft: 600}} />
+      <Link to='/' className="center brand-logo">
+      Student KYC</Link>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><Link to='/students'>Student's Data</Link></li>
       </ul>
